749c0e06a1991204a2a0ffbd5df1297afb189d70
okedo <viktorpatraboi@gmail.com>
2023-07-13 12:22:51 +0300

    small change

--- src/main/java/com/spikecorp/authserver/service/UserService.java
+++ src/main/java/com/spikecorp/authserver/service/UserService.java
@@ -7,6 +7,7 @@ import com.spikecorp.authserver.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -14,15 +15,11 @@ import java.util.List;
 @AllArgsConstructor
 @Service
 public class UserService {
-    private UserRepository repository;
+    private final UserRepository repository;
 
     public User createUser(@RequestBody User user) {
-        Boolean noUserName = user.getUsername() == null || user.getUsername().trim().isEmpty();
-        Boolean noPassword = user.getPassword() == null || user.getPassword().trim().isEmpty();
-        Boolean noEmail = user.getEmail() == null || user.getEmail().trim().isEmpty();
-        if (noUserName || noPassword || noEmail) {
-            throw new IllegalArgumentException("incorrect data");
-        }
+        validateUserData(user);
+
         try {
             return repository.save(user);
         } catch (DataIntegrityViolationException ex) {
@@ -42,28 +39,45 @@ public class UserService {
         User existingUser = repository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException("No user with the specified ID"));
 
-        if (updatedUser.getUsername() != null && !updatedUser.getUsername().isEmpty()
-                && !existingUser.getUsername().equals(updatedUser.getUsername())) {
-            validateUniqueUsername(updatedUser.getUsername(), id);
-            existingUser.setUsername(updatedUser.getUsername());
-        }
-
-        if (updatedUser.getEmail() != null && !updatedUser.getEmail().isEmpty()
-                && !existingUser.getEmail().equals(updatedUser.getEmail())) {
-            validateUniqueEmail(updatedUser.getEmail(), id);
-            existingUser.setEmail(updatedUser.getEmail());
-        }
-
-        if (updatedUser.getPassword() != null && !updatedUser.getPassword().isEmpty()
-                && !existingUser.getPassword().equals(updatedUser.getPassword())) {
-            existingUser.setPassword(updatedUser.getPassword());
-        }
+        updateUsername(existingUser, updatedUser);
+        updateEmail(existingUser, updatedUser);
+        updatePassword(existingUser, updatedUser);
 
         return repository.save(existingUser);
     }
 
     public void deleteUser(Long id) {
         repository.deleteById(id);
+    }
+
+    private void validateUserData(User user) {
+        if (ObjectUtils.isEmpty(user.getUsername()) || ObjectUtils.isEmpty(user.getPassword())
+                || ObjectUtils.isEmpty(user.getEmail())) {
+            throw new IllegalArgumentException("Incorrect data");
+        }
+    }
+
+    private void updateUsername(User existingUser, User updatedUser) {
+        String updatedUsername = updatedUser.getUsername();
+        if (!ObjectUtils.isEmpty(updatedUsername) && !existingUser.getUsername().equals(updatedUsername)) {
+            validateUniqueUsername(updatedUsername, existingUser.getId());
+            existingUser.setUsername(updatedUsername);
+        }
+    }
+
+    private void updateEmail(User existingUser, User updatedUser) {
+        String updatedEmail = updatedUser.getEmail();
+        if (!ObjectUtils.isEmpty(updatedEmail) && !existingUser.getEmail().equals(updatedEmail)) {
+            validateUniqueEmail(updatedEmail, existingUser.getId());
+            existingUser.setEmail(updatedEmail);
+        }
+    }
+
+    private void updatePassword(User existingUser, User updatedUser) {
+        String updatedPassword = updatedUser.getPassword();
+        if (!ObjectUtils.isEmpty(updatedPassword) && !existingUser.getPassword().equals(updatedPassword)) {
+            existingUser.setPassword(updatedPassword);
+        }
     }
 
     private void validateUniqueUsername(String username, Long id) {
